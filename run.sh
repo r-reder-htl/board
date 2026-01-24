6275cb6ac42ff775ad8f62549d729490b8c6c3af
robreder <r.reder@gmx.at>
2026-01-24 11:29:31 +0100

    changed folder for board-solo

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 VERSION=0.9.0
+
 mkdir -p repo/at/htl/board/board/${VERSION}/
-mkdir -p repo/at/htl/board-solo/board-solo/${VERSION}/
+mkdir -p repo/at/htl/board/board-solo/${VERSION}/
+
 cp ../Board/board-${VERSION}.jar ./repo/at/htl/board/board/${VERSION}/.
 cp ../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board/board/${VERSION}/.
-cp ../Board/board-solo-${VERSION}.jar ./repo/at/htl/board-solo/board-solo/${VERSION}/.
-cp ../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board-solo/board-solo/${VERSION}/board-solo-${VERSION}-javadoc.jar
+cp ../Board/board-solo-${VERSION}.jar ./repo/at/htl/board/board-solo/${VERSION}/.
+cp ../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board/board-solo/${VERSION}/board-solo-${VERSION}-javadoc.jar
 
