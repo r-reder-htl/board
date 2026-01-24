78128ae9fc2606edc46238c595ce47f6c566b5e5
robreder <r.reder@gmx.at>
2026-01-24 10:13:43 +0100

    added version 0.9.0 and javadoc

---
+++ run.sh
@@ -0,0 +1,4 @@
+VERSION=0.9.0
+mkdir -p repo/at/htl/board/board/${VERSION}/
+cp ../../Board/board-${VERSION}.jar ./repo/at/htl/board/board/${VERSION}/.
+cp ../../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board/board/${VERSION}/.
