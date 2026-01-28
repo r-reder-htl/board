3b9fccc5f275af40cd93fb71d256b823a2cd1532
r-reder-htl <r.reder@htl-leonding.ac.at>
2026-01-28 14:28:02 +0100

    added signature

--- run.sh
+++ run.sh
@@ -5,6 +5,16 @@ mkdir -p repo/at/htl/board/board-solo/${VERSION}/
 
 cp ../Board/board-${VERSION}.jar ./repo/at/htl/board/board/${VERSION}/.
 cp ../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board/board/${VERSION}/.
+BACK_FOLDER=`pwd`
+cd ./repo/at/htl/board/board/${VERSION}/
+sha1sum board-${VERSION}.jar > board-${VERSION}.jar.sha1
+sha1sum board-${VERSION}.pom > board-${VERSION}.pom.sha1
+cd $BACK_FOLDER
+ 
 cp ../Board/board-solo-${VERSION}.jar ./repo/at/htl/board/board-solo/${VERSION}/.
 cp ../Board/board-${VERSION}-javadoc.jar ./repo/at/htl/board/board-solo/${VERSION}/board-solo-${VERSION}-javadoc.jar
+cd ./repo/at/htl/board/board-solo/${VERSION}/
+sha1sum board-solo-${VERSION}.jar > board-solo-${VERSION}.jar.sha1
+sha1sum board-solo-${VERSION}.pom > board-solo-${VERSION}.pom.sha1
+cd $BACK_FOLDER
 
